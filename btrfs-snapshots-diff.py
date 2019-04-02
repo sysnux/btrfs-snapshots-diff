1fa85210fd822cd6d10b71c6a7f53e875f3d62f3
Marcel Schnirring <mschnirring@marcel-schnirring.de>
2019-04-02 22:56:42 +0200

    Improved symlink and data handling

--- btrfs-snapshots-diff.py
+++ btrfs-snapshots-diff.py
@@ -88,9 +88,8 @@ class BtrfsStream(object):
         attr, l_attr = unpack('<HH', self.stream[index:index + self.l_tlv])
         if self.send_attrs[attr] != attr_type:
             raise ValueError('Unexpected attribute %s' % self.send_attrs[attr])
-        # XXX l_attr])
-        ret, = unpack('<H', self.stream[
-                      index + self.l_tlv:index + self.l_tlv + 2])
+        ret = unpack('<%dB' % l_attr, self.stream[
+                      index + self.l_tlv:index + self.l_tlv + l_attr])
         return index + self.l_tlv + l_attr, ret
 
     def _tlv_get_string(self, attr_type, index):
@@ -113,7 +112,7 @@ class BtrfsStream(object):
         attr, l_attr = unpack('<HH', self.stream[index:index + self.l_tlv])
         if self.send_attrs[attr] != attr_type:
             raise ValueError('Unexpected attribute %s' % self.send_attrs[attr])
-        ret = unpack('<' + 'B' * self.BTRFS_UUID_SIZE,
+        ret = unpack('<%dB' % self.BTRFS_UUID_SIZE,
                      self.stream[index + self.l_tlv:index + self.l_tlv + l_attr])
         return index + self.l_tlv + l_attr, ''.join(['%02x' % x for x in ret])
 
@@ -159,11 +158,12 @@ class BtrfsStream(object):
             elif command == 'BTRFS_SEND_C_SYMLINK':
                 idx2, path = self._tlv_get_string(
                     'BTRFS_SEND_A_PATH', idx + self.l_head)
-                # XXX BTRFS_SEND_A_PATH_LINK in send-self.stream.c ???
-                idx2, ino = self._tlv_get_string('BTRFS_SEND_A_INO', idx2)
+                idx2, ino = self._tlv_get_u64('BTRFS_SEND_A_INO', idx2)
+                idx2, path_link = self._tlv_get_string(
+                    'BTRFS_SEND_A_PATH_LINK', idx2)
                 modified.setdefault(path, []).append(
                     (command[13:].lower(), count))
-                commands.append((command[13:].lower(), ino))
+                commands.append((command[13:].lower(), path_link))
 
             elif command == 'BTRFS_SEND_C_LINK':
                 idx2, path = self._tlv_get_string(
@@ -348,8 +348,10 @@ if __name__ == "__main__":
 
     if args.parent is not None:
         if args.child is not None:
+            # TODO add option to ommit '--no-data'
+            stream_file = '/tmp/snaps-diff'
             cmd = ['btrfs', 'send', '-p', args.parent, '--no-data',
-                   '-f', '/tmp/snaps-diff', args.child]
+                   '-f', stream_file, args.child]
             try:
                 subprocess.check_call(cmd)
 
@@ -357,7 +359,6 @@ if __name__ == "__main__":
                 printerr('Error: %s\nexecuting "%s"\n' %
                          (exc_info()[0], ' '.join(cmd)))
                 exit(1)
-            stream_file = '/tmp/snaps-diff'
         else:
             printerr('Error: parent needs child!\n')
             parser.print_help()
@@ -461,8 +462,8 @@ if __name__ == "__main__":
                     'snapshot: uuid=%s, ctrasid=%d, clone_uuid=%s, clone_ctransid=%d' % cmd[1:])
 
             elif a[0] == 'write':
-                # XXX cmd[2] is data, but what does it represent?
                 print_actions.append('write: from %d' % cmd[1])
+                print_actions.append('data: \n' + ''.join([chr(c) for c in cmd[2]]))  # convert bytes to string
 
             else:
                 print_actions.append('%s, %s %s' % (a, cmd, '-' * 20))
